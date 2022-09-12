b21643d0a8d809380c61942d3f6aa982f0386189
Ekaterinsl <107034416+Ekaterinsl@users.noreply.github.com>
2022-09-12 20:25:06 +0400

    Update Program.cs

--- 6_3/Program.cs
+++ 6_3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Text;
 
@@ -15,7 +15,6 @@ namespace _6_3
                 Console.WriteLine("Введите 2 - чтобы заполнить данные");
                 Key = Console.ReadKey();
                 Console.WriteLine();
-
                 switch (Key.KeyChar)
                 {
                     case '1':
@@ -33,51 +32,43 @@ namespace _6_3
         }
         static void Input()
         {
-            if (!File.Exists(@"e:\rabs.txt"))
+            if (!File.Exists(@"e:\rabs.csv"))
             {
-                File.Create(@"e:\rabs.txt").Close();
+                File.Create(@"e:\rabs.csv").Close();
                 Console.WriteLine("Файл создан");
             }
-            using (StreamWriter sw = new StreamWriter("rabs.txt", true, Encoding.Unicode))
+            using (StreamWriter sw = new StreamWriter(@"e:\rabs.csv", true, Encoding.Unicode))
             {
-                char Key;
+                char Key = 'y';
                 do
                 {
                     string file = string.Empty;
                     Console.Write("\nВведите ID:  ");
                     file += $"{Console.ReadLine()}\t";
-
                     string now = DateTime.Now.ToString();
                     Console.Write($"Дата и время добавления записи: {now} ");
                     file += $"{now}\t";
-
                     Console.Write("\nВведите ФИО:   ");
                     file += $"{Console.ReadLine()}\t";
-
                     Console.Write("\nВведите возраст:   ");
                     file += $"{Console.ReadLine()}\t";
-
                     Console.Write("\nВведите рост:   ");
                     file += $"{Console.ReadLine()}\t";
-
                     Console.Write("\nВведите дату рождения:   ");
                     file += $"{Console.ReadLine()}\t";
-
                     Console.Write("\nВведите место рождения:   ");
                     file += $"{Console.ReadLine()}\t";
-
+                    sw.WriteLine(file);
                     Console.Write("Продолжить n/y"); Key = Console.ReadKey(true).KeyChar;
-
                 } while (char.ToLower(Key) == 'y');
             }
         }
         static void Print()
         {
-            using (StreamReader sr = new StreamReader(@"e:\rabs.txt", Encoding.Unicode))
+            using (StreamReader sr = new StreamReader(@"e:\rabs.csv", Encoding.Unicode))
             {
                 string line;
                 Console.WriteLine($"{"ID",5}{"Дата и время",20}{"ФИО",15}{"Возраст",15}{"Рост",15}{"Дата рождения",15}{"Место рождения",20}");
-
                 while ((line = sr.ReadLine()) != null)
                 {
                     string[] data = line.Split('\t');
